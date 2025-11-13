33d7a6f751441b898c6ebf204f652f8b4dca7900
Timur <timurhay1@gmail.com>
2025-11-13 19:38:32 +0300

    sprint5 f_a siftUp

--- src/sprint5/final_task_a.kt
+++ src/sprint5/final_task_a.kt
@@ -1,5 +1,7 @@
 package sprint5
 
+import java.util.Collections
+
 class Person(
     val name: String,
     val completedTasks: Int,
@@ -14,6 +16,24 @@ class Person(
     }
 
     override fun compareTo(other: Person): Int = comparator.compare(this, other)
+}
+
+fun <T : Comparable<T>> heapSort(heap: MutableList<T>): MutableList<T> {
+    tailrec fun siftUp(heap: MutableList<T>, index: Int) {
+        if (index == 1) {
+            return
+        }
+        val parentIndex = index / 2
+
+        if (heap[parentIndex] > heap[index]) {
+            Collections.swap(heap, parentIndex, index)
+            return siftUp(heap, parentIndex)
+        }
+    }
+
+    fun siftDown(){
+
+    }
 }
 
 fun main() {
