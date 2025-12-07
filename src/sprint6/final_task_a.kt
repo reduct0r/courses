6df7da7c718b4cbdb16de0dd46187b0be312f54b
Timur <timurhay1@gmail.com>
2025-12-08 00:55:22 +0300

    sprint6 f_a end

--- src/sprint6/final_task_a.kt
+++ src/sprint6/final_task_a.kt
@@ -1,4 +1,35 @@
-package sprint6_fa
+// https://contest.yandex.ru/contest/25070/run-report/152976449/
+
+/**
+-- ПРИНЦИП РАБОТЫ --
+    Я реализовал адаптированный алгоритм Дейкстры для поиска максимального остовного дерева (maximum spanning tree).
+    Граф представлен матрицей смежности, где хранится максимальный вес ребра между вершинами (если несколько - берётся max).
+    Начинаем с вершины 0, инициализируя расстояния (longestDistToVertex) как MIN_PATH_LEN, кроме стартовой - 0.
+    Пока есть непосещённые вершины с ключом > MIN:
+        Выбираем вершину v с максимальным ключом, добавляем в дерево (visited=true).
+        Для каждого непосещённого соседа u обновляем ключ, если вес ребра > текущего ключа u.
+        Сумма расстояний - вес максимального остовного дерева.
+        Если не все вершины посещены - граф несвязный, выводим "Oops! I did it again".
+
+-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ --
+    Алгоритм следует жадному свойству для максимального остовного дерева:
+    на каждом шаге добавляется максимальное ребро, соединяющее дерево с внешней вершиной, без циклов.
+    Обновление ключей только для непосещённых обеспечивает отсутствие циклов.
+    Выбор max ребра среди возможных гарантирует оптимальность для жадных алгоритмов.
+    Обработка нескольких рёбер между одинаковыми парами вершин берёт max.
+
+-- ВРЕМЕННАЯ СЛОЖНОСТЬ --
+    Построение графа: O(m), где m - рёбра.
+    Алгоритм MaxST: O(n^2) - для n вершин, каждый раз просматриваем n для поиска max вершины (getMaxNotVisitedVertex),
+    и n для обновления соседей.
+    Общая сложность O(n^2) в худшем случае.
+
+-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --
+    Матрица смежности: O(n^2).
+    Массивы visited и longestDistToVertex: O(n).
+    Список весов: O(n).
+    Итого O(n^2).
+ */
 
 const val MIN_PATH_LEN = Long.MIN_VALUE / 2
 
@@ -45,8 +76,12 @@ fun main() {
         graph.addEdge(v - 1, u - 1, w)
     }
 
-    //print(findMaxSpanTree(graph).last())
-    findMaxSpanTree(graph).forEach { print("$it ") }
+    val maxSpanTreeWeight = findMaxSpanTree(graph).sum()
+    if (graph.visited.contains(false)) {
+        print("Oops! I did it again")
+    } else {
+        print(maxSpanTreeWeight)
+    }
 }
 
 fun findMaxSpanTree(graph: Graph): MutableList<Long> {
@@ -62,8 +97,8 @@ fun findMaxSpanTree(graph: Graph): MutableList<Long> {
 
         for (u in 0 until graph.numOfVertex) {
             val w = graph.matrix[v * graph.numOfVertex + u] ?: continue
-            val newDist = graph.longestDistToVertex[v] + w.toLong()
-            if (newDist > graph.longestDistToVertex[u]) {
+            val newDist = w.toLong()
+            if (newDist > graph.longestDistToVertex[u] && !graph.visited[u]) {
                 graph.longestDistToVertex[u] = newDist
             }
         }
