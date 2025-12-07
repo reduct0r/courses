a8f67f2e5d1a5090a1ae94ad35e726c0e097ade7
Timur <timurhay1@gmail.com>
2025-12-07 23:03:42 +0300

    sprint6 try

---
+++ src/sprint6/dijkstra.kt
@@ -0,0 +1,119 @@
+package sprint6_my
+
+class Graph(
+    val numOfVertex: Int
+) {
+    private val matrix = MutableList<Int?>(numOfVertex * numOfVertex) { null }
+    val visited = MutableList(numOfVertex) { false }
+    val shortestDistToVertex = MutableList(numOfVertex) { Int.MAX_VALUE }
+
+    fun addEdge(v: Int, u: Int, weight: Int) {
+        if (matrix[v * numOfVertex + u] == null) {
+            matrix[v * numOfVertex + u] = weight
+            matrix[u * numOfVertex + v] = weight
+        } else {
+            throw IllegalArgumentException()
+        }
+    }
+
+    fun getOutWithWeight(v: Int): MutableList<Pair<Int, Int?>> {
+        val list = mutableListOf<Pair<Int, Int?>>()
+        for (i in 0 until numOfVertex) {
+            list.add(Pair(i, matrix[v * numOfVertex + i]))
+        }
+        return list
+    }
+
+    fun setVisited(v: Int) {
+        visited[v] = true
+    }
+
+    fun getNumberOfEdges(): Int {
+        return matrix.count { it != null }
+    }
+
+    fun getMinNotVisitedVertex(): Int? {
+        var currentMin = Int.MAX_VALUE
+        var minVertex: Int? = null
+        for (i in 0 until numOfVertex) {
+            if (!visited[i] && shortestDistToVertex[i] < currentMin) {
+                currentMin = shortestDistToVertex[i]
+                minVertex = i
+            }
+        }
+        return minVertex
+    }
+
+    fun getShortestDist(v: Int): Int {
+        return shortestDistToVertex[v]
+    }
+
+    fun matrixToSB(nullPath: String = "0"): StringBuilder {
+        val builder = StringBuilder()
+        for (i in 0 until numOfVertex) {
+            for (j in 0 until numOfVertex) {
+                val value = matrix[i * numOfVertex + j]
+                builder.append(value?.toString() ?: nullPath)
+                builder.append(" ")
+            }
+            builder.appendLine()
+        }
+        return builder
+    }
+
+    fun pathsToString(nullPath: String = "-1"): StringBuilder {
+        val builder = StringBuilder()
+        for (i in 0 until numOfVertex) {
+            for (j in 0 until numOfVertex) {
+                val value = distMatrix[i][j]
+                builder.append(if (value >= INF) nullPath else value.toString())
+                builder.append(" ")
+            }
+            builder.appendLine()
+        }
+        return builder
+    }
+
+}
+
+fun main() {
+    val reader = System.`in`.bufferedReader()
+    val (n, m) = reader.readLine().split(" ").map { it.toInt() }
+    val graph = Graph(n)
+
+    repeat(m) {
+        val (v, u, w) = reader.readLine().split(" ").map { it.toInt() }
+        graph.addEdge(v - 1, u - 1, w)
+    }
+
+    for (i in 0 until n) {
+        dijkstra(graph, i)
+    }
+
+    print (graph.matrixToSB().toString())
+}
+
+fun dijkstra(graph: Graph, start: Int) {
+    graph.shortestDistToVertex[start] = 0
+
+    while (true) {
+        val v = graph.getMinNotVisitedVertex()
+        if (v == null || graph.getShortestDist(v) == Int.MAX_VALUE) {
+            break
+        }
+
+        graph.setVisited(v)
+        val out = graph.getOutWithWeight(v)
+
+        for ((u, w) in out) {
+            if (w != null) {
+                val newDist = graph.getShortestDist(v) + w
+                if (newDist < graph.getShortestDist(u)) {
+                    graph.shortestDistToVertex[u] = newDist
+                }
+            }
+        }
+
+    }
+}
+
