0676c35ca4c0ed17475a7a8381625eebdfda35f8
Reduc0r <timurhay1@gmail.com>
2025-12-22 00:35:39 +0300

    sprint6 f_a fix

--- src/sprint6/final_task_a.kt
+++ src/sprint6/final_task_a.kt
@@ -1,4 +1,4 @@
-// https://contest.yandex.ru/contest/25070/run-report/152976449/
+// https://contest.yandex.ru/contest/25070/run-report/154168730/
 
 /**
 -- ПРИНЦИП РАБОТЫ --
@@ -31,24 +31,21 @@
     Итого O(n^2).
  */
 
-const val MIN_PATH_LEN = Long.MIN_VALUE / 2
+const val MIN_PATH_LEN = Int.MIN_VALUE / 2
 
 class Graph(
     val numOfVertex: Int
 ) {
-    val matrix: Array<Int?> = arrayOfNulls(numOfVertex * numOfVertex)
+    val matrix: Array<Array<Int?>> = Array(numOfVertex) { arrayOfNulls(numOfVertex) }
     val visited: BooleanArray = BooleanArray(numOfVertex)
-    val longestDistToVertex: LongArray = LongArray(numOfVertex) { MIN_PATH_LEN }
-    val maxSpanTreeWeights = mutableListOf<Long>()
+    val longestDistToVertex: IntArray = IntArray(numOfVertex) { MIN_PATH_LEN }
 
     fun addEdge(v: Int, u: Int, weight: Int) {
-        val path = matrix[v * numOfVertex + u]
-        if (path == null) {
-            matrix[v * numOfVertex + u] = weight
-            matrix[u * numOfVertex + v] = weight
-        } else if (path < weight) {
-            matrix[v * numOfVertex + u] = weight
-            matrix[u * numOfVertex + v] = weight
+        when {
+            matrix[v][u] == null || matrix[v][u]!! < weight -> {
+                matrix[v][u] = weight
+                matrix[u][v] = weight
+            }
         }
     }
 
@@ -63,47 +60,41 @@ class Graph(
         }
         return maxVertex
     }
-
 }
 
 fun main() {
     val reader = System.`in`.bufferedReader()
     val (n, m) = reader.readLine().split(" ").map { it.toInt() }
     val graph = Graph(n)
-
     repeat(m) {
         val (v, u, w) = reader.readLine().split(" ").map { it.toInt() }
         graph.addEdge(v - 1, u - 1, w)
     }
-
-    val maxSpanTreeWeight = findMaxSpanTree(graph).sum()
-    if (graph.visited.contains(false)) {
+    val (maxSpanTreeWeight, visitedCount) = findMaxSpanTree(graph)
+    if (visitedCount < n) {
         print("Oops! I did it again")
     } else {
         print(maxSpanTreeWeight)
     }
 }
 
-fun findMaxSpanTree(graph: Graph): MutableList<Long> {
+fun findMaxSpanTree(graph: Graph): Pair<Long, Int> {
     val start = 0
-    graph.longestDistToVertex[start] = 0L
-
+    graph.longestDistToVertex[start] = 0
+    var sum = 0L
+    var visitedCount = 0
     while (true) {
-        val v = graph.getMaxNotVisitedVertex()
-        if (v == null || graph.longestDistToVertex[v] == MIN_PATH_LEN) {
-            break
-        }
+        val v = graph.getMaxNotVisitedVertex() ?: break
         graph.visited[v] = true
-
+        visitedCount++
+        sum += graph.longestDistToVertex[v].toLong()
         for (u in 0 until graph.numOfVertex) {
-            val w = graph.matrix[v * graph.numOfVertex + u] ?: continue
-            val newDist = w.toLong()
-            if (newDist > graph.longestDistToVertex[u] && !graph.visited[u]) {
-                graph.longestDistToVertex[u] = newDist
+            if (graph.visited[u]) continue
+            val w = graph.matrix[v][u] ?: continue
+            if (w > graph.longestDistToVertex[u]) {
+                graph.longestDistToVertex[u] = w
             }
         }
-        graph.maxSpanTreeWeights.add(graph.longestDistToVertex[v])
     }
-
-    return graph.maxSpanTreeWeights
+    return sum to visitedCount
 }
